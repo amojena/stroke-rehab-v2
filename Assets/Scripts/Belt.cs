f4981110e4c3649a7ba8aabcd61bc7f156089cb1
amojena <am3238@cornell.edu>
2020-09-30 13:11:14 -0400

    Not all items behave equally, crates added w/o functionality

---
+++ Assets/Scripts/Belt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fruitOrigin;
+
+    [SerializeField]
+    GameObject endOfBelt;
+
+    [SerializeField]
+    float spawnDelay;
+
+    [SerializeField]
+    float speedFactor;
+
+    [SerializeField]
+    GameObject[] items;
+
+
+    float spawnTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTime >= spawnDelay)
+        {
+            SpawnItem();
+            spawnTime = 0.0f;
+        }
+
+        spawnTime += Time.deltaTime;
+        
+    }
+
+    void SpawnItem()
+    {
+        int randomIndex = Random.Range(0, 1000) % items.Length;
+        GameObject itemToSpawn = items[randomIndex];
+        Instantiate(itemToSpawn, fruitOrigin.transform.position, itemToSpawn.transform.rotation);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.gameObject.transform.position = Vector3.MoveTowards(other.gameObject.transform.position, endOfBelt.transform.position, speedFactor * Time.deltaTime);
+        Debug.Log(other.gameObject.name);
+    }
+}
